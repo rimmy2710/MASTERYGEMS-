0d27676fcdd830047b18d9d3f96778d7bffb6f78
rimmy2710 <duymatviet2710@gmail.com>
2025-11-28 16:27:24 +0700

    Update hardhat.config.ts

--- masterygems/contracts/hardhat.config.ts
+++ masterygems/contracts/hardhat.config.ts
@@ -1,10 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const MONAD_RPC_URL = process.env.MONAD_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     hardhat: {},
+    monadMainnet: {
+      url: MONAD_RPC_URL,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
   },
 };
 
